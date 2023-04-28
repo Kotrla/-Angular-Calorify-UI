ab2a9b5f61aa812e96c92837e15bb743de67ecaf
Kristian <kristikotrla@live.com>
2023-04-28 05:24:09 +0200

    Created foundation of the landing page

---
+++ calorify/src/app/components/landing/modules/landing-feature.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { landingRoutes } from '../utils/landing.routes';
+import { LandingFeatureWithoutRoutingModule } from './landing-feature-without-routing.module';
+
+@NgModule({
+  imports: [
+    LandingFeatureWithoutRoutingModule,
+    RouterModule.forChild(landingRoutes)
+  ]
+})
+export class LandingFeatureModule { }
--- calorify/src/app/modules/app-routing.module.ts
+++ calorify/src/app/modules/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '', loadChildren: () => import('../components/landing/modules/landing-feature.module').then(m => m.LandingFeatureModule) },
+    { path: 'home', loadChildren: () => import('../components/home/modules/home-feature.module').then(m => m.HomeFeatureModule) },
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
