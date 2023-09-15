1eb32b5be650aef8d1d9864e771611c992c60093
Kristian <kristikotrla@live.com>
2023-09-15 14:15:35 +0200

    Added sections to /home

---
+++ calorify/src/app/components/home/components/weekly-summary/weekly-summary.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-weekly-summary',
+  templateUrl: './weekly-summary.component.html',
+  styleUrls: ['./weekly-summary.component.scss']
+})
+export class WeeklySummaryComponent {
+
+}
